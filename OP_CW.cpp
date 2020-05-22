a521f45c717ca2167c5185ec428770c35e7b4581
Dmytro Geleshko <dimahunter17@gmail.com>
2020-05-22 14:56:28 +0200

    Added second method

--- OP_CW.cpp
+++ OP_CW.cpp
@@ -13,6 +13,28 @@ int main()
 	double diff;
 }
 
+int** getNullM(int rows, int cols)
+{
+	int** res = new int* [rows];
+	for (int i = 0; i < rows; i++)
+	{
+		res[i] = new int[cols];
+		for (int j = 0; j < cols; j++)
+		{
+			res[i][j] = 0;
+		}
+	}
+	return res;
+}
+void deleteM(int** Matrx, int rows)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] Matrx[i];
+	}
+	delete[] Matrx;
+}
+
 double* Jacobi( double** A,  double* B,  double* X,int size,double diff) //Need to test with double X as B
 {
 	double* tempX = new double[size];
@@ -36,24 +58,26 @@ double* Jacobi( double** A,  double* B,  double* X,int size,double diff) //Need 
 	} while (norm > diff);
 	delete[] tempX;
 }
-int** getNullM(int rows,int cols)
+double* G_Z(double** A, double* B, double* X, int size, double diff) //Need to test with double X as B
 {
-	int** res = new int*[rows];
-	for (int i = 0; i < rows; i++)
-	{
-		res[i] = new int[cols];
-		for (int j = 0; j < cols; j++)
-		{
-			res[i][j] = 0;
+	double* tempX = new double[size];
+	double norm;
+
+	do {
+		for (int i = 0; i < size; i++) {
+			tempX[i] = X[i];						//tempx(newX)=Old X
+			for (int j = 0; j < size; j++) {
+				if (i != j)
+					tempX[i] -= A[i][j] * tempX[j];	// As we go we use the new parameters
+			}
+			tempX[i] /= A[i][i];
 		}
-	}
-	return res;
-}
-void deleteM(int** Matrx,int rows)
-{
-	for (int i = 0; i < rows; i++)
-	{
-		delete[] Matrx[i];
-	}
-	delete[] Matrx;
+		norm = fabs(X[0] - tempX[0]);
+		for (int i = 0; i < size; i++) {
+			if (fabs(X[i] - tempX[i]) > norm)
+				norm = fabs(X[i] - tempX[i]);
+			X[i] = tempX[i];
+		}
+	} while (norm > diff);
+	delete[] tempX;
 }
